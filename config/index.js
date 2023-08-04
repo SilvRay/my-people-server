75f5c2131354cdbe9c006281da3a2b07e62a8d66
Nicolas_M <nicolas.meot@live.fr>
2023-08-04 23:50:48 +0200

    llelel

--- config/index.js
+++ config/index.js
@@ -24,7 +24,8 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: "*",
+      origin :"*"
+      //origin: ["*","http://localhost:3000", process.env.ORIGIN],
     })
   );
 
